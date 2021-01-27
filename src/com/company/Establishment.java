62c188a9574192d5671daa8b27f16ad8292678d4
pavlodemborynskyi <benlex00@gmail.com>
2021-01-27 13:40:05 +0200

    commit 3.Establishment updated

--- src/com/company/Establishment.java
+++ src/com/company/Establishment.java
@@ -4,78 +4,93 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Establishment {
-    private String name,description,adress,contacts;
+    private String name,description,address,contacts;
+    private City city;
     private String[] imagelist;
-    private int workHours;
+    private String workHours;
     private double rating;
-    private List<Table> ourtables;
+    private List<Table> tables;
 
 
-    public Establishment(String name,String description,String adress, String contacts,String[]imgList,
-                         int workHours,double rating,List<Table> ourtables) {
+    public Establishment(String name,String description,String address, String contacts,City city,String[] imageList,
+                         String workHours,double rating,List<Table> tables) {
         this.name = name;
         this.description = description;
-        this.adress = adress;
+        this.address = address;
         this.contacts = contacts;
-        this.imagelist = imgList;
+        this.imagelist = imageList;
         this.workHours = workHours;
+        this.city = city;
         this.rating = rating;
-        this.ourtables=ourtables;
+        this.tables= tables;
     }
 
-    public String getE_Name(){
-            return name;
+    public String getName(){
+            return this.name;
     }
-    public void setE_Name(String Name){
-        name = Name;
+    public void seName(String newName){
+        this.name = newName;
     }
 
 
-    public void setDescription(String Desc){
-        description = Desc;
+    public void setDescription(String newDesc){
+        this.description = newDesc;
     }
     public String getDescription(){
-        return description;
+        return this.description;
     }
 
 
-    public void setAdress(String addr){
-        adress = addr;
+    public void setAddress(String newAdress){
+        this.address = newAdress;
     }
-    public String getAdress(){
-        return adress;
+    public String getAddress(){
+        return this.address;
     }
 
 
-    public void setContacts(String Contacts){
-        contacts = Contacts;
+    public void setContacts(String newContacts){
+        this.contacts = newContacts;
     }
     public String getContacts(){
-        return contacts;
+        return this.contacts;
     }
 
 
-    public void setWorkHours(int wh){
-        workHours = wh;
+    public void setWorkHours(String newWH){
+        this.workHours = newWH;
     }
-    public int getWorkHours(){
-        return workHours;
-    }
-
-    public void setRating(double rat){
-        rating = rat;
+    public String getWorkHours(){
+        return this.workHours;
     }
 
+    public void setRating(double newRate){
+        this.rating = newRate;
+    }
     public double getRating(){
-        return rating;
+        return this.rating;
     }
 
-    public List<Table> getOurtables() {
-        return ourtables;
+    public List<Table> getTables() {
+        return this.tables;
+    }
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
     }
 
-    public void setOurtables(List<Table> ourtables) {
-        this.ourtables = ourtables;
+    public City getCity() {
+        return city;
     }
 
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String[] getImagelist() {
+        return imagelist;
+    }
+
+    public void setImagelist(String[] imagelist) {
+        this.imagelist = imagelist;
+    }
 }
